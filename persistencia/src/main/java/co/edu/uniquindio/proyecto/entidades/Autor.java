89659833c3e3c393bd797e7ffba203386aa9fc18
Sebastian Morales Duque <smoralesd@uqvirtual.edu.co>
2021-10-01 21:03:28 -0500

    se agrega la libreria lombok

--- persistencia/src/main/java/co/edu/uniquindio/proyecto/entidades/Autor.java
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/entidades/Autor.java
@@ -1,5 +1,7 @@
 package co.edu.uniquindio.proyecto.entidades;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,57 +11,22 @@ import java.util.IdentityHashMap;
 import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@ToString
 public class Autor implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Integer codigo;
     private String nombre;
     private Integer anionacimiento;
 
-    public Autor(){
-        super();
-    }
-
     public Autor(String nombre, Integer anionacimiento) {
         this.nombre = nombre;
         this.anionacimiento = anionacimiento;
-    }
-
-    public Integer getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(Integer codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Integer getAnionacimiento() {
-        return anionacimiento;
-    }
-
-    public void setAnionacimiento(Integer anionacimiento) {
-        this.anionacimiento = anionacimiento;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Autor autor = (Autor) o;
-        return Objects.equals(codigo, autor.codigo);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(codigo);
     }
 }
